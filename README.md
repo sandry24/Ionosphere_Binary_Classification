cd5e5f4e98742580bee8511ed1bab8e01fa21738
sandry24 <sandry.grosu.24@gmail.com>
2023-10-07 19:42:22 +0300

    Initial Commit

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# binary_classification
+# Ionosphere Binary Classification
+Basic model that trains on a dataset about ionosphere and learns to predict, using given data, state good or bad. 
  
---
+++ ion_binary_classification.py
@@ -0,0 +1,64 @@
+import pandas as pd
+from tensorflow import keras
+from keras import layers, callbacks
+import matplotlib.pyplot as plt
+
+ion = pd.read_csv('ion.csv', index_col=0)
+print(ion.head())
+
+df = ion.copy()
+df['Class'] = df['Class'].map({'good': 0, 'bad': 1})
+
+df_train = df.sample(frac=0.7, random_state=0)
+df_valid = df.drop(df_train.index)
+
+max_ = df_train.max(axis=0)
+min_ = df_train.min(axis=0)
+
+df_train = (df_train - min_) / (max_ - min_)
+df_valid = (df_valid - min_) / (max_ - min_)
+df_train.dropna(axis=1, inplace=True)
+df_valid.dropna(axis=1, inplace=True)
+
+X_train = df_train.drop('Class', axis=1)
+X_valid = df_valid.drop('Class', axis=1)
+y_train = df_train['Class']
+y_valid = df_valid['Class']
+
+early_stopping = callbacks.EarlyStopping(
+    patience=10,
+    min_delta=0.001,
+    restore_best_weights=True,
+)
+
+model = keras.Sequential([
+    layers.Dense(4, activation='relu', input_shape=[33]),
+    layers.Dense(4, activation='relu'),
+    layers.Dense(1, activation='sigmoid'),
+])
+
+model.compile(
+    optimizer='adam',
+    loss='binary_crossentropy',
+    metrics=['binary_accuracy'],
+)
+
+history = model.fit(
+    X_train, y_train,
+    validation_data=(X_valid, y_valid),
+    batch_size=512,
+    epochs=1000,
+    callbacks=[early_stopping],
+    verbose=False,
+)
+
+history_df = pd.DataFrame(history.history)
+history_df.loc[5:, ['loss', 'val_loss']].plot()
+history_df.loc[5:, ['binary_accuracy', 'val_binary_accuracy']].plot()
+
+print(f"Best Validation Loss: {history_df['val_loss'].min()}")
+print(f"Best Validation Accuracy: {history_df['val_binary_accuracy'].max()}")
+plt.show()
+
+
+
